653efcef9139a72ad7c347a5f21ad2ba44223048
Matt <matthew-casey@lambdastudents.com>
2021-05-27 22:36:19 -0500

    got rid of the white border around the entire page.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import styles from 'styled-components'
 const apiUrl= 'https://api.nasa.gov/planetary/apod?api_key=cvicckb2zf4QgnYkFf0SkSDeEJdIs6M8URg70k9K';
 
 const Background = styles.div`
-background-image: url(https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2020/07/shutterstock_262946390.jpg?w=750);
+background-color: gray;
 background-size: cover;
 min-height: 100vh;
 min-width: 100vh;
-background-position: fixed;
-
+display: flex;
+align-items: center;
+justify-content: space-around;
+box-sizing: border-box
 `
 
 function App() {
