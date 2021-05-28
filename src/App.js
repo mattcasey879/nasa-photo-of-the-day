950b5466e4c829cd2b5712a32ac9a00f07d792fe
Matt <matthew-casey@lambdastudents.com>
2021-05-27 22:20:08 -0500

    added style componets to file for day 2 module 4 project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Info from './Components/Info';
-import SearchBar from './Components/SearchBar';
+import styles from 'styled-components'
 
-const apiUrl= 'https://api.nasa.gov/planetary/apod?api_key=cvicckb2zf4QgnYkFf0SkSDeEJdIs6M8URg70k9K'
+const apiUrl= 'https://api.nasa.gov/planetary/apod?api_key=cvicckb2zf4QgnYkFf0SkSDeEJdIs6M8URg70k9K';
+
+const Background = styles.div`
+background-image: url(https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2020/07/shutterstock_262946390.jpg?w=750);
+background-size: cover;
+min-height: 100vh;
+min-width: 100vh;
+background-position: fixed;
+
+`
 
 function App() {
 const [info, setInfo] = useState({})
@@ -18,10 +27,9 @@ useEffect(() => {
 },[])
 console.log(info)
   return (
-    <div className="App">
-      <SearchBar />
+    <Background className="App">
       <Info data={info}/>
-    </div>
+    </Background>
   );
 }
 
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,17 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styles from 'styled-components';
 
-const Info = props => {
-  const { url, explanation, title } = props.data
+const TopContent = styles.div`
+text-align: center;
+font-family: Oswald, sans-serif;
+margin: 0;
+letter-spacing: 3px;
+h2{
+  font-size: 2.5rem;
+}
+p{
+  font-size: 1.25rem;
+  color: black;
+  font-weight: bold;
+}
+span{
+  font-size: 2rem;
+}
+`
+const Info = ({data}) => {
   return (
-      <div className='info-container'>
-        <div className='top-content'>
-          <h2 className='vid-title'>{title}</h2>
-          <section className='img-container'>
-            <iframe src={url} 
-                    title='Nasa APOD video'/>
+      <div>
+        <TopContent>
+          <h2>{data.title}</h2>
+          <span>{data.date}</span>
+          <section>
+            <img src={data.url} alt='nasa-APOD img of the day'/>
+            <span>&#169;{data.copyright}</span>
           </section>
-          <p>{explanation}</p>
-        </div>
+          <p>{data.explanation}</p>
+        </TopContent>
 
       </div>
   )
